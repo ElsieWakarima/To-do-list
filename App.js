6f4562a83e6093de5dc279e0e13b39bd9c0c28f8
ElsieWakarima <elsiewakarima52@gmail.com>
2023-02-10 15:34:51 +0300

    Test

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { TailwindProvider } from 'tailwindcss-react-native';
+// import Homepagescreen from './components/screens/Homepagescreen';
+
+// export default function App() {
+//   const Stack = createNativeStackNavigator();
+//   // const Drawer = createDrawerNavigator();
+
+//   return (
+//     <NavigationContainer >
+//         <TailwindProvider>
+
+//     <Stack.Navigator  initialRouteName="Homepagescreen">
+//     <Stack.Screen options={{headerShown: false}}name="Homepagescreen" component={Homepagescreen} />
+  
+      
+
+
+
+//     </Stack.Navigator>
+   
+//     </TailwindProvider>
+//   </NavigationContainer>
+    
+  
+//   );
+// }
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ components/screens/Homepagescreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Homepagescreen = () => {
+  return (
+    <View>
+      <Text>Homepagescreen</Text>
+    </View>
+  )
+}
+
+export default Homepagescreen
